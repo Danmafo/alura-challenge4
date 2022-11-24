eb0ecae75de0053e5d4b23fa0df630e9b8cac6fa
Daniel Fonseca <daniel_mf@live.com>
2022-11-23 23:40:46 -0300

    Mudando path da api e mapeando entidades

---
+++ src/main/java/br/com/daniel/challenge/models/Receita.java
@@ -0,0 +1,67 @@
+package br.com.daniel.challenge.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "tb_receitas")
+public class Receita {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private BigDecimal valor;
+
+    @Temporal(TemporalType.DATE)
+    private Date data;
+
+    public Receita() {
+    }
+
+    public Receita(Long id, String descricao, BigDecimal valor, Date data) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+}
